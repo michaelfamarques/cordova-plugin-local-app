25b48ec5696a91b8f453b0aa531f29501e6c421e
Michael Fagundes <michael.famarques@gmail.com>
2022-03-15 19:45:13 -0300

    Fix file path

--- www/index.js
+++ www/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,8 +8,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { gt } from 'semver';
-export class LocalApp {
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LocalApp = void 0;
+const semver_1 = require("semver");
+class LocalApp {
     constructor(params) {
         this.params = params;
     }
@@ -27,7 +30,7 @@ export class LocalApp {
     checkNewVersionExists() {
         return __awaiter(this, void 0, void 0, function* () {
             let localVersion, localBuildNumber, isLocalInstaled;
-            if (this.currentLocalVersion && gt(this.currentLocalVersion, this.params.currentVersion)) {
+            if (this.currentLocalVersion && (0, semver_1.gt)(this.currentLocalVersion, this.params.currentVersion)) {
                 localVersion = this.currentLocalVersion;
                 localBuildNumber = this.currentLocalBuildNumber;
                 isLocalInstaled = true;
@@ -43,12 +46,12 @@ export class LocalApp {
                 if (!lastVersion) {
                     throw new Error('Nenhuma versão existente online!');
                 }
-                if (gt(lastVersion.version, localVersion)) {
+                if ((0, semver_1.gt)(lastVersion.version, localVersion)) {
                     yield this.installVersion(lastVersion, false);
                     localVersion = lastVersion.version;
                     isLocalInstaled = true;
                 }
-                else if (gt(lastVersion.version, localVersion) && lastVersion.build > localBuildNumber) {
+                else if ((0, semver_1.gt)(lastVersion.version, localVersion) && lastVersion.build > localBuildNumber) {
                     yield this.installVersion(lastVersion, true);
                     localVersion = lastVersion.version;
                     isLocalInstaled = true;
@@ -217,3 +220,4 @@ export class LocalApp {
         console.error(args);
     }
 }
+exports.LocalApp = LocalApp;
